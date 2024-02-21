9ffd0519c75a7e504ac00bfa1fdd88bcf75a00c2
MichalSliwaSE <112940119+MichalSliwaSE@users.noreply.github.com>
2024-02-21 21:55:29 +0100

    Yada

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -2,6 +2,6 @@ function Message() {
   const name = "Michal";
   if (name) {
     return <h1>Hello {name}</h1>;
-  } else return <h1>Hello World</h1>;
+  } else return <h1>Hellos World</h1>;
 }
 export default Message;
