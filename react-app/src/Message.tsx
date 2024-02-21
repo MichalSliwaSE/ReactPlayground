e12e946dab5e9659a42d0231cba632c516026504
MichalSliwaSE <112940119+MichalSliwaSE@users.noreply.github.com>
2024-02-21 22:28:58 +0100

    Goodnight

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,7 @@
 function Message() {
   const name = "Michal";
   if (name) {
-    return <h1>Hello {name}</h1>;
+    return <h1>Hellos {name}</h1>;
   } else return <h1>Hellos World</h1>;
 }
 export default Message;
